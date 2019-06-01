f4530dbd0d78fd48d5d78bd0bc36576f439984f4
Salaah <salaah_amin@hotmail.co.uk>
2019-06-01 03:46:17 +0100

    Layout arrangement

--- gui 2/frontend.py
+++ gui 2/frontend.py
@@ -12,21 +12,41 @@ class Application(tk.Frame):
     def frame_layouts(self):
         # File Uplaod Section (Top)
         self.frame_file_upload = tk.Frame(self.window)
-        self.frame_file_upload.grid(row=0, column=0)
+        self.frame_file_upload.grid(
+            row=0,
+            column=0,
+            pady=(5, 10),
+            padx=(5, 0),
+            sticky='w, e'
+            )
 
         # File List
         self.frame_file_list = tk.Frame(self.window)
-        self.frame_file_list.grid(row=1, column=0)
+        self.frame_file_list.grid(
+            row=1,
+            column=0,
+            padx=(5, 0),
+            pady=(0, 5),
+            sticky='w, e, n, s')
 
         # Optimise Options
         self.frame_optimise_opts = tk.Frame(self.window)
-        self.frame_optimise_opts.grid(row=2, column=0)
+        self.frame_optimise_opts.grid(
+            row=1,
+            column=1,
+            padx=20
+            )
 
     def file_upload(self):
         # Input
         file_path = tk.StringVar()
         ent_file = tk.Entry(self.frame_file_upload, textvariable=file_path)
-        ent_file.grid(row=0, column=0)
+        ent_file.grid(
+            row=0,
+            column=0,
+            padx=(0, 5),
+            sticky='w, e'
+            )
 
         # Browse Button
         browse_btn = tk.Button(
@@ -38,65 +58,135 @@ class Application(tk.Frame):
 
     def file_list(self):
         files = tk.Listbox(self.frame_file_list, height=6, width=35)
-        files.grid(row=0, column=0)
+        files.grid(
+            row=0,
+            column=0,
+            sticky='w, e, n, s'
+            )
 
     def optimise_opts(self):
         # Auto Optimise
-        lbl_auto = tk.Label(self.frame_optimise_opts, text="Auto Optimise")
-        lbl_auto.grid(row=0, column=0, columnspan=3)
-        opt_auto = tk.Checkbutton(self.frame_optimise_opts)
-        opt_auto.grid(row=0, column=3, sticky='w')
+        opt_auto = tk.Checkbutton(
+                        self.frame_optimise_opts,
+                        text="Auto Optimise"
+                        )
+        opt_auto.grid(
+            row=0,
+            column=0,
+            pady=(0, 15),
+            columnspan=4
+            )
 
         # Quality
         lbl_quality = tk.Label(self.frame_optimise_opts, text='Quality')
-        lbl_quality.grid(row=1, column=0, pady=(20, 0))
+        lbl_quality.grid(
+            row=1,
+            column=0,
+            pady=(0, 0)
+            )
+
+        val_quality = tk.IntVar()
         opt_quality = tk.Scale(
-                            self.frame_optimise_opts,
-                            from_=0,
-                            to=100,
-                            orient='horizontal',
-                            )
-        opt_quality.grid(row=1, column=1, columnspan=3, sticky='w')
+            self.frame_optimise_opts,
+            from_=0,
+            to=100,
+            orient='horizontal',
+            variable=val_quality,
+            )
+        opt_quality.grid(
+            row=1,
+            column=1,
+            pady=(0, 15),
+            columnspan=3
+            )
 
         # Resize
         val_resize_w = tk.StringVar()
         lbl_resize_w = tk.Label(self.frame_optimise_opts, text='W')
-        lbl_resize_w.grid(row=3, column=0)
+        lbl_resize_w.grid(
+            row=3,
+            column=0,
+            pady=(0, 15)
+            )
         opt_resize_w = tk.Entry(
-                            self.frame_optimise_opts,
-                            width=5,
-                            textvariable=val_resize_w
-                            )
-        opt_resize_w.grid(row=3, column=1, sticky='w')
+            self.frame_optimise_opts,
+            width=5,
+            textvariable=val_resize_w
+            )
+        opt_resize_w.grid(
+            row=3,
+            column=1,
+            pady=(0, 15)
+            )
 
         val_resize_h = tk.StringVar()
         lbl_resize_h = tk.Label(self.frame_optimise_opts, text='H')
-        lbl_resize_h.grid(row=3, column=2, sticky='w')
+        lbl_resize_h.grid(
+            row=3,
+            column=2,
+            pady=(0, 15)
+            )
         opt_resize_h = tk.Entry(
                             self.frame_optimise_opts,
                             width=5,
                             textvariable=val_resize_h
                             )
-        opt_resize_h.grid(row=3, column=3, sticky='w')
+        opt_resize_h.grid(
+            row=3,
+            column=3,
+            pady=(0, 15)
+            )
 
         # Convert
         val_convert = tk.StringVar()
         convert_opts = ['(none)', 'JPEG', 'PNG', 'GIF', 'BMP']
         val_convert.set(convert_opts[0])
-        lbl_convert = tk.Label(self.frame_optimise_opts, text="Convert Format")
-        lbl_convert.grid(row=4, column=0, columnspan=2)
+        lbl_convert = tk.Label(self.frame_optimise_opts, text="Format")
+        lbl_convert.grid(
+            row=4,
+            column=0,
+            pady=(0, 15),
+            columnspan=1
+            )
         opt_convert = tk.OptionMenu(
                                 self.frame_optimise_opts,
                                 val_convert,
                                 *convert_opts
                                 )
-        opt_convert.grid(row=4, column=2, columnspan=2)
+        opt_convert.grid(
+            row=4,
+            column=1,
+            pady=(0, 15),
+            columnspan=3,
+            sticky='w, e'
+            )
+
+        # Optimise Button
+        btn_optimise = tk.Button(
+            self.frame_optimise_opts,
+            text='Optimse'
+        )
+        btn_optimise.grid(row=5, column=0, columnspan=4)
 
     def build(self):
+
+        # Call Layounds
         self.frame_layouts()
+
+        # Call Sections
         self.file_upload()
         self.file_list()
         self.optimise_opts()
+
+        # Layout Rules to Fit Screen on Resize
+        self.window.columnconfigure(0, weight=1)
+        self.window.rowconfigure(1, weight=1)
+        self.frame_file_upload.rowconfigure(0, weight=1)
+        self.frame_file_upload.columnconfigure(0, weight=1)
+        self.frame_file_list.rowconfigure(0, weight=1)
+        self.frame_file_list.columnconfigure(0, weight=1)
+        self.frame_optimise_opts.rowconfigure(0, weight=1)
+        self.frame_optimise_opts.columnconfigure(0, weight=1)
         self.mainloop()
 
 if __name__ == "__main__":
